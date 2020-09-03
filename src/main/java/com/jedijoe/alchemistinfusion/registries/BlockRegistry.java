a6589343b559e233e0cefe2156ae99f35ccf947c
jedijoe93 <jedijoe93@gmail.com>
2020-09-03 14:21:44 -0400

    Initial commit

---
+++ src/main/java/com/jedijoe/alchemistinfusion/registries/BlockRegistry.java
@@ -0,0 +1,17 @@
+package com.jedijoe.alchemistinfusion.registries;
+
+import com.jedijoe.alchemistinfusion.AlchemistInfusion;
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BlockRegistry {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AlchemistInfusion.MOD_ID);
+
+    public static void init(){
+    BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+}
---
+++ src/main/java/com/jedijoe/alchemistinfusion/registries/RegistryController.java
@@ -0,0 +1,9 @@
+package com.jedijoe.alchemistinfusion.registries;
+
+public class RegistryController {
+
+    public static void LaunchItems(){
+        //list all registries here with their .init functions
+        BlockRegistry.init();
+    }
+}
